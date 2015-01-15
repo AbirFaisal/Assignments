c063d4bd09b55cf4d04e5c3dfd4eada5709cde6d
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-01-15 15:08:42 -0500

    COP2800
    COP2800 Java Assignments

---
+++ HelloWorld/src/com/abirFaisal/helloWorld.java
@@ -0,0 +1,10 @@
+package com.abirFaisal;
+
+/**
+ * Created by abirfaisal on 1/15/15.
+ */
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
