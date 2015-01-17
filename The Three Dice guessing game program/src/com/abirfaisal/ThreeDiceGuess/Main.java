91a2864b2f2bab073434867a2e129331ad152fb3
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-01-16 19:32:02 -0500

    Final

--- The Three Dice guessing game program/src/com/abirfaisal/ThreeDiceGuess/Main.java
+++ The Three Dice guessing game program/src/com/abirfaisal/ThreeDiceGuess/Main.java
@@ -20,22 +20,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean keepPlaying = true;
-        int wins = 0;
-        int lastRoll = Dice.Roll(3);
+        boolean keepPlaying = true;     //keep playing or not
+        int wins = 0;                   //keep track of wining streaks
+        int lastRoll = Dice.Roll(3);    //store the last dice roll and perform the initial roll
 
         while (keepPlaying == true) {
             System.out.println("Last Roll: " + lastRoll);
             System.out.println("Current Streak: " + wins);
             System.out.println("Will the next roll be higher, lower or the same?");
-            System.out.println("1 - Higher ");
-            System.out.println("2 - Lower ");
-            System.out.println("3 - Same");
+            System.out.println("Enter one of the following options:");
+            System.out.println("[1] Higher ");
+            System.out.println("[2] Lower ");
+            System.out.println("[3] Same");
+            System.out.println("[4] Quit");
             Scanner input = new Scanner(System.in);
             int answer = input.nextInt();
 
+            //Option 1
             if (answer == 1) {
-                int newRoll = Dice.Roll(3);     //perform a new dice roll
+                int newRoll = Dice.Roll(3);     //perform 3 new dice rolls
                 if (newRoll > lastRoll) {       //compare rolls
                     lastRoll = newRoll;         //make lastRoll = newRoll
                     wins = wins + 1;            //add to wins streak
@@ -43,7 +46,10 @@ public class Main {
                     lastRoll = newRoll;         //if compare is false then reset win steak
                     wins = 0;                   // and make lastRoll = newRoll
                 }
-            } else if (answer == 2) {
+            }
+
+            //Option 2 no difference from option 1 besides the operand in the if statement.
+            else if (answer == 2) {
                 int newRoll = Dice.Roll(3);
                 if (newRoll < lastRoll) {        // swapped around the < so we can check if user guessed correctly
                     lastRoll = newRoll;
@@ -52,7 +58,9 @@ public class Main {
                     lastRoll = newRoll;
                     wins = 0;
                 }
-            } else if (answer == 3) {
+            }
+            //Option 3 no difference from option 1 besides the operand in the if statement.
+            else if (answer == 3) {
                 int newRoll = Dice.Roll(3);
                 if (newRoll == lastRoll) {       // changed to == so see if newRoll is the same as lastRoll
                     lastRoll = newRoll;
@@ -61,18 +69,29 @@ public class Main {
                     lastRoll = newRoll;
                     wins = 0;
                 }
-            } else if (answer == 4) {
+            }
+
+            //If user enters 4 set keepPlaying to false to end the game.
+            else if (answer == 4) {
                 keepPlaying = false;
-            } else {
+            }
+
+            //If user enters invalid number then this will let the user know.
+            else if (answer > 4){
                 System.out.println("Invalid Input Fuck You");
+            }
+
+            //clear the console with 50 blank lines.
+            //I'm sure there is a better way to do this but it works so whatever.
+            for (int i = 0; i < 50; i++) {
+                System.out.println();
             }
         }
 
+        //If user enters option 4 then this if statement will become true.
         if (keepPlaying == false) {
             System.out.println("Thanks for playing");   //Thank user for playing
             System.exit(0);                             //Quit
         }
-
-
     }
 }
