858fc11d247b4c4588f197fd623c2878809cebd6
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-02-02 19:12:30 -0500

    initial commit for new assignment

---
+++ Playing with 1D array Assignment/src/com/abirfaisal/Main.java
@@ -0,0 +1,25 @@
+package com.abirfaisal;
+/*
+Write a program that does the following:
+	•	Allows the user to determine how many random number between 1 and 100 are to be stored.  In other words the user could pick 10,500.  You would generate and store 10,500 numbers randomly generated all between 1 and 100.
+	•	Declare an array that stores the random numbers (total determined by user).
+	•	Print out the array
+	•	Sort the array
+	•	Print out the array
+	•	Search the array for a number between 1 and 100…display found or not found
+	•	Display each number from 1 to 100 and the number of times found in the array
+	•	Display the average of the numbers in the array
+	•	Display the highest number in the array
+	•	Display the lowest number in the array
+	•	Everything should be done using METHODS
+	•	Design tool = Yes
+	•	Put up in Blackboard = Yes
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
