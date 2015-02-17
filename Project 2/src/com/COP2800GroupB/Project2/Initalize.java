785f297f590c4e0735fbd33948b06e6aaf2c6db7
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-02-16 20:40:48 -0500

    Removed unused code

--- Project 2/src/com/COP2800GroupB/Project2/Initalize.java
+++ Project 2/src/com/COP2800GroupB/Project2/Initalize.java
@@ -16,18 +16,10 @@ public class Initalize {
 
 
     //Create a constant size for each database we want to create.
-    static final int MAX_MANAGERS = 20;
-    static final String MANAGER_FILE_NAME = "Manager";
-
-    static final int MAX_EMPLOYEES = 100;
-    static final String EMPLOYEE_FILE_NAME = "Employee";
-
     static final int MAX_PERSONS = 500;
     static final String PERSON_FILE_NAME = "Person";
 
     //Create database refrences for ea
-    static Database[] managers = new Database[MAX_MANAGERS - 1];
-    static Database[] employee = new Database[MAX_EMPLOYEES - 1];
     static Database[] person = new Database[MAX_PERSONS - 1];
 
 
@@ -37,27 +29,17 @@ public class Initalize {
 
 
         // Create empty database objects for use
-        for (int i = 0; i < (MAX_MANAGERS-1); i++) {
-            managers[i] = new Database();
-        }
-        for (int i = 0; i < (MAX_EMPLOYEES-1); i++) {
-            employee[i] = new Database();
-
-        }
         for (int i = 0; i < (MAX_PERSONS-1); i++) {
             person[i] = new Database();
         }
 
-        initalizeDatabase(managers, MANAGER_FILE_NAME, MAX_MANAGERS);
-        initalizeDatabase(employee, EMPLOYEE_FILE_NAME, MAX_EMPLOYEES);
         initalizeDatabase(person, PERSON_FILE_NAME, MAX_PERSONS);
 
 
         // check if database exists
         // if exists then load into specified array
         // if does not exist create it
-        checkAndCreate(managers, MANAGER_FILE_NAME);
-        checkAndCreate(employee, EMPLOYEE_FILE_NAME);
+
         checkAndCreate(person, PERSON_FILE_NAME);
 
 
