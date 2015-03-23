7c9a8e440bdbd34b1e5e285de6f0c26f36dba460
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-03-23 17:38:40 -0400

    imported methods

---
+++ Project 3/src/com/GroupB/Edit.java
@@ -0,0 +1,11 @@
+package com.GroupB;
+
+/**
+ * Created by abirfaisal on 3/23/15.
+ */
+public class Edit {
+
+
+
+
+}
---
+++ Project 3/src/com/GroupB/PrintRecords.java
@@ -0,0 +1,70 @@
+package com.GroupB;
+
+/**
+ * Created by abirfaisal on 3/23/15.
+ */
+public class PrintRecords {
+
+    public static void print(Person[] personArray){
+
+
+
+        //Determine spacing based on name longest length
+        //String to hold spaces
+        String space = "";
+        //Get number of spaces to generate
+        int spaceIterations = Name.getConcatNameLength(personArray);
+        //Add spaces
+        for (int i = 0; i < spaceIterations; i++) {
+
+            space = space.concat(" ");
+
+        }
+
+
+
+
+
+
+
+
+        //Print Header
+        System.out.print("# Name" + space + " Gender  " + "Salary \n");
+
+        //Go through array and print each record
+        for (int i = 0; i < personArray.length; i++) {
+
+            //Determine spacing for record based on record length
+            //String to hold spaces
+            String recordSpace = "";
+            //Get number of spaces to generate
+
+            //store lengths
+            int tempfirst = personArray[i].getName().getFirstName().length();
+            int tempMiddle = personArray[i].getName().getMiddleName().length();
+            int tempLast = personArray[i].getName().getLastName().length();
+
+            //record name length
+            int recordLength = tempfirst + tempMiddle + tempLast;
+            //calculate amount of space between name and gender
+            int recordSpaceIterations = spaceIterations - recordLength + 3;
+            //Add spaces
+            for (int d = 0; d < recordSpaceIterations; d++) {
+                recordSpace = recordSpace.concat(" ");
+            }
+
+            //Print out record
+            System.out.printf("%d %s %s %s" + recordSpace + "%s  $%s \n",
+                    (i+1),
+                    personArray[i].getName().getFirstName(),
+                    personArray[i].getName().getMiddleName(),
+                    personArray[i].getName().getLastName(),
+                    genderToMaleFemale(i, personArray),
+                    personArray[i].getSalary());
+        }
+    }
+
+
+
+
+}
