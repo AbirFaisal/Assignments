a9aa18425bd6601aac93f8e49b32292e8b1e336f
AbirFaisal <abirfaisal@Abirs-MBP.lan>
2015-04-05 20:56:16 -0400

    added classes

--- Homework-Ch-12/src/com/abirfaisal/HWChTwelve/CH12_2/CH12_2.java
+++ Homework-Ch-12/src/com/abirfaisal/HWChTwelve/CH12_2/CH12_2.java
@@ -1,7 +1,30 @@
 package com.abirfaisal.HWChTwelve.CH12_2;
 
+import java.util.Scanner;
+
 /**
- * Created by abirfaisal on 4/5/15.
+ HW 12.2 (InputMismatchException)
+ Write a program that prompts the user to read two integers and displays their sum.
+ Your program should prompt the user to read the number again if the input is incorrect.
  */
+
+
 public class CH12_2 {
+
+
+    public static void start(){
+
+        Scanner input = new Scanner(System.in);
+        int inta;
+        int intb;
+        
+
+        System.out.print("HW 12.2 Input Mismatch Exception");
+
+
+
+    }
+
+
+
 }
--- Homework-Ch-12/src/com/abirfaisal/HWChTwelve/Main.java
+++ Homework-Ch-12/src/com/abirfaisal/HWChTwelve/Main.java
@@ -1,19 +1,20 @@
 package com.abirfaisal.HWChTwelve;
 
-/*
-HW 12.2
-(InputMismatchException) Write a program that prompts the user to read two integers and displays their sum.
-Your program should prompt the user to read the number again if the input is incorrect.
+/**
 
-HW 12.3
-(ArrayIndexOutOfBoundsException) Write a program that meets the following requirements:
+ HW 12.2
+ (InputMismatchException) Write a program that prompts the user to read two integers and displays their sum.
+ Your program should prompt the user to read the number again if the input is incorrect.
+
+ HW 12.3
+ (ArrayIndexOutOfBoundsException) Write a program that meets the following requirements:
  - Creates an array with 100 randomly chosen integers.
  - Prompts the user to enter the index of the array, then displays the corresponding element value.
  - If the specified index is out of bounds, display the message Out of Bounds.
 
-HW 12.14
-(Process scores in a text file) Suppose that a text file contains an unspecified number of scores separated by blanks.
-Write a program that prompts the user to enter the file, reads the scores from the file, and displays their total and average.
+ HW 12.14
+ (Process scores in a text file) Suppose that a text file contains an unspecified number of scores separated by blanks.
+ Write a program that prompts the user to enter the file, reads the scores from the file, and displays their total and average.
 
 
  */
@@ -25,15 +26,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        if (args.length > 0){
+            printLines(50);
+            System.out.println("Invalid Input");
+        }
 
+        Scanner input = new Scanner(System.in);
 
         System.out.print("1. Homework: 12.2 \n" +
                          "2. Homework: 12.3 \n" +
                          "3. Homework: 12.14 \n" +
-                         "Select an Assignment:");
+                         "q to quit \n" +
+                         "Select an Assignment: ");
+        String selection = input.next();
 
 
+        switch (selection.charAt(0)){
+            case '1':
 
+                break;
+            case '2':
+
+                break;
+            case '3':
+
+                break;
+            case ('q' | 'Q'):
+                break;
+            default: main(new String[]{"1"});
+        }
+    }
+
+
+    public static void printLines(int lines){
+
+        String line = "";
+
+        for (int i = 0; i < lines; i++) {
+            line = line + "\n";
+        }
+
+        System.out.print(line);
     }
 }
