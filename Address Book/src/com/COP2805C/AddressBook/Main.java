3ea44544f157908244f96bbb3a6c9251980030c0
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-05-28 15:56:23 -0400

    basic JavaFX application

--- Address Book/src/com/COP2805C/AddressBook/Main.java
+++ Address Book/src/com/COP2805C/AddressBook/Main.java
@@ -1,8 +1,19 @@
 package com.COP2805C.AddressBook;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+
+
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 }
