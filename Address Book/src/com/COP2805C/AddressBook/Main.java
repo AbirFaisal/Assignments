c07c868030455625437c4882d97e0606406f1852
AbirFaisal <abirfaisal@Abirs-MBP.lan>
2015-06-08 00:06:48 -0400

    almost done with main window

--- Address Book/src/com/COP2805C/AddressBook/Main.java
+++ Address Book/src/com/COP2805C/AddressBook/Main.java
@@ -3,12 +3,19 @@ package com.COP2805C.AddressBook;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.List;
@@ -32,10 +39,103 @@ public class Main extends Application {
 
 
 
-        ObservableList<String> contactsObservableList = new FXCollections();
-        ListView<String> contactListView = new ListView<>(contactsObservableList);
+        //TODO somthing here? This is a Sample
+        //Information
+        //TODO TEST REMOVE
+        Text phoneNumberText = new Text("nigger");
+        Text emailText = new Text("nigger");
+        Text addressText = new Text("nigger");
+        Text groupText = new Text("nigger");
 
 
+        TextArea notesTextArea = new TextArea();
+        notesTextArea.setPrefSize(400, 400);
+        notesTextArea.setPromptText("Notes");
+
+
+
+        //Flowpane to format the text nicey
+        FlowPane contactInfoFlowPane = new FlowPane();
+        contactInfoFlowPane.setVgap(15.0);
+        contactInfoFlowPane.setOrientation(Orientation.VERTICAL);
+
+
+
+        //TEST Remove
+        contactInfoFlowPane.getChildren().addAll(phoneNumberText, emailText, addressText, groupText, notesTextArea);
+
+
+
+        //Scroll pane incase of large amount of data
+        ScrollPane contactInfoScrollPane = new ScrollPane(contactInfoFlowPane);
+        AnchorPane.setTopAnchor(contactInfoScrollPane, 120.0);
+        AnchorPane.setBottomAnchor(contactInfoScrollPane, 0.0);
+        AnchorPane.setLeftAnchor(contactInfoScrollPane, 0.0);
+        AnchorPane.setRightAnchor(contactInfoScrollPane, 0.0);
+
+
+
+
+        //Contact Photo
+        ImageView contactPhotoImageView = new ImageView();
+        //Image contactImage = new Image("default.jpg");
+        //contactPhotoImageView.setImage(contactImage);
+
+
+        //First Middle Last name text label
+        Text contactNameText = new Text("nigger");
+        contactNameText.strokeTypeProperty().set(StrokeType.OUTSIDE);
+        contactNameText.setTextAlignment(TextAlignment.CENTER);
+        contactNameText.setFont(Font.font(24.0));
+
+
+
+        //Top banner with photo and name
+        FlowPane bannerFlowPane = new FlowPane(contactPhotoImageView, contactNameText);
+        bannerFlowPane.setAlignment(Pos.CENTER);
+        bannerFlowPane.setColumnHalignment(HPos.CENTER);
+        AnchorPane.setTopAnchor(bannerFlowPane, 20.0);
+        AnchorPane.setLeftAnchor(bannerFlowPane, 20.0);
+        AnchorPane.setRightAnchor(bannerFlowPane, 20.0);
+
+
+
+        //Right side Anchor Pane
+        AnchorPane rightAnchorPane = new AnchorPane(bannerFlowPane, contactInfoScrollPane);
+        AnchorPane.setTopAnchor(rightAnchorPane, 0.0);
+        AnchorPane.setBottomAnchor(rightAnchorPane, 0.0);
+        AnchorPane.setLeftAnchor(rightAnchorPane, 0.0);
+        AnchorPane.setRightAnchor(rightAnchorPane, 0.0);
+
+
+
+
+
+
+
+
+        //TODO add menu items
+        SplitMenuButton menuButton = new SplitMenuButton();
+        //TODO MenuItems here
+
+
+
+        //Group selection
+        //TODO some shit here
+        ChoiceBox<String> groupChoiceBox = new ChoiceBox<>();
+        //ObservableList<String> groupObservableList =
+        //groupChoiceBox.setItems();
+
+
+        //Search Box
+        //TODO set anchors
+        TextField searchTextField = new TextField();
+
+
+        //Contact List
+        //TODO set anchors
+        ListView<String> contactListView = new ListView<String>();
+        ObservableList<String> contactObservableList = FXCollections.observableArrayList ();
 
 
         //Left side Anchor Pane
@@ -46,12 +146,6 @@ public class Main extends Application {
         AnchorPane.setRightAnchor(leftAnchorPane, 0.0);
 
 
-        //Right side Anchor Pane
-        AnchorPane rightAnchorPane = new AnchorPane();
-        AnchorPane.setTopAnchor(rightAnchorPane, 0.0);
-        AnchorPane.setBottomAnchor(rightAnchorPane, 0.0);
-        AnchorPane.setLeftAnchor(rightAnchorPane, 0.0);
-        AnchorPane.setRightAnchor(rightAnchorPane, 0.0);
 
 
         //Split Pane
