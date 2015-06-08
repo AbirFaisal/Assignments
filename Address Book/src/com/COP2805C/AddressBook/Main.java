a27ea52283a470a13c7dc93e5413dc1bb819b863
AbirFaisal <AbirFaisal@users.noreply.github.com>
2015-06-08 00:39:31 -0400

    Update Main.java

--- Address Book/src/com/COP2805C/AddressBook/Main.java
+++ Address Book/src/com/COP2805C/AddressBook/Main.java
@@ -8,28 +8,6 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-<<<<<<< HEAD
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-=======
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.shape.StrokeType;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
-
-import java.util.List;
->>>>>>> AbirFaisal
-
-public class Main extends Application {
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -42,13 +20,9 @@ import javafx.stage.Stage;
 
 import java.util.List;
 
+
 public class Main extends Application {
 
-<<<<<<< HEAD
-public class Main extends Application {
-
-=======
->>>>>>> AbirFaisal
     List<Contact> contactList;
 
     public static void main(String[] args) {
@@ -62,59 +36,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
 
-       //------------------------------------------------------ Right Panel Content
 
-        TextArea textAreaContent = new TextArea();
-        textAreaContent.setPrefSize(360, 200);
-
-        ImageView imageContentHolder = new ImageView(); //IDK Fill in later
-        imageContentHolder.setFitHeight(100);
-        imageContentHolder.setFitWidth(100);
-
-        FlowPane flowPanelArea = new FlowPane(); //ImageConenetHolder is in this area and there are fonts in this area to, but they are abstract classes
-        flowPanelArea.setPrefSize(360, 200);
-
-        FlowPane flowPanelArea2 = new FlowPane(); //Same as flowPanelArea
-
-
-        //----------------------------------------------------- Right Panel Content
-
-
-
-
-       //------------------------------------------------------- Left Panel Content
-        MenuItem menuItemContent2 = new MenuItem();
-        MenuItem menuItemContent = new MenuItem();
-
-        SplitMenuButton splitMenuButton = new SplitMenuButton(menuItemContent, menuItemContent2);
-
-        ChoiceBox choiceBoxSelector = new ChoiceBox();
-        choiceBoxSelector.setPrefSize(103, 26);
-
-        TextField viewTextField = new TextField();
-        viewTextField.setPrefSize(168, 26);
-
-        ListView contactListview = new ListView();
-        contactListview.setPrefSize(200, 200);
-        //------------------------------------------------------- Left Panel Content
-
-        AnchorPane rightAnchorPane = new AnchorPane();
-
-        AnchorPane leftAnchorPane = new AnchorPane();
-        leftAnchorPane.setPrefSize(100, 160);
-
-        SplitPane splitPane = new SplitPane(leftAnchorPane, rightAnchorPane);
-        splitPane.setPrefSize(400, 400);
-
-        AnchorPane anchorPane = new AnchorPane(splitPane);
-        anchorPane.setPrefSize(800, 600);
-
-
-<<<<<<< HEAD
-        Scene primaryScene = new Scene(anchorPane);
-
-=======
->>>>>>> AbirFaisal
 
         //TODO somthing here? This is a Sample
         //Information
