8579e974fe5b83b1241ee8a30b8871ac3de1d432
Chris Buruchian <EpiphX@Christophers-MacBook-Pro.local>
2015-06-24 16:49:57 -0400

    Added createCONTACTID method

--- Address Book/src/com/COP2805C/AddressBook/Main.java
+++ Address Book/src/com/COP2805C/AddressBook/Main.java
@@ -86,6 +86,8 @@ public class Main extends Application {
         @Override
         public void start(Stage primaryStage)throws Exception {
 
+            //TODO build method that repopulates contacts depending on which group is selected.
+            //TODO load groups from database into an ArrayList.
             if (populateContacts) {
                 try {
                     contactIDS = database.getContactIDS(credentials);
@@ -94,6 +96,7 @@ public class Main extends Application {
                     for (int begin = 0; begin < contactIDS.size(); begin++) {
                         contactInformationArrayList.add(cib.prepareContact(contactIDS.get(begin)));
                     }
+
                     //Below: For testing
                     System.out.println(contactInformationArrayList.toString());
                 } catch (NullPointerException e) {
